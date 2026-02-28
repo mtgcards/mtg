6ab70f64d80a12c91bcd44bfec61bdd6515c0265
mtgcards <se_034@yahoo.co.jp>
2026-02-28 15:17:55 +0900

    mod

---
+++ src/app/manifest.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next';
+import { SITE_NAME } from '@/lib/constants';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: SITE_NAME,
+    short_name: '昭和MTG',
+    icons: [
+      { src: '/web-app-manifest-192x192.png', sizes: '192x192', type: 'image/png' },
+      { src: '/web-app-manifest-512x512.png', sizes: '512x512', type: 'image/png' },
+    ],
+    display: 'browser',
+  };
+}
