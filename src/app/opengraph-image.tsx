28db6d45c8d3b21e2a73d532b0531b0cedce2d54
mtgcards <se_034@yahoo.co.jp>
2026-03-02 18:36:45 +0900

    fix: add dynamic=force-static to root opengraph-image.tsx

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,6 +1,7 @@
 import { DEFAULT_FORMAT, FORMAT_DESCRIPTIONS, FORMAT_PAGE_TITLES, SITE_NAME, TAB_LABELS } from '@/lib/constants';
 import { buildOgImage, OG_CONTENT_TYPE, OG_SIZE } from '@/lib/og';
 
+export const dynamic = 'force-static';
 export const size = OG_SIZE;
 export const contentType = OG_CONTENT_TYPE;
 export const alt = SITE_NAME;
